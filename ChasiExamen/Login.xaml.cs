594322d1379da3f18bd8335713c9a959caf73596
Alex Chasi <alexchasi17@gmail.com>
2021-10-26 19:23:11 -0500

    creado ventana de Registro

--- ChasiExamen/Login.xaml.cs
+++ ChasiExamen/Login.xaml.cs
@@ -16,9 +16,39 @@ namespace ChasiExamen
         {
             InitializeComponent();
         }
-
-        private void btnIniciar_Clicked(object sender, EventArgs e)
+        //agregamos (async) el hilo de conexion entre ventanas
+        private async void btnIniciar_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                //almacenamos en variable los datos del usuario ingresado 
+                string usuario = txtUsuario.Text;
+
+                /*
+                //permite abrir la ventana de notas (MainPage)
+                await Navigation.PushAsync(new MainPage(usuario));
+                */
+
+                //login validado
+                if (txtUsuario.Text == "estudiante2021" && txtContrasenia.Text == "uisrael2021")
+                {
+                    //permite abrir la ventana de notas (MainPage)
+                    await Navigation.PushAsync(new MainPage(usuario));
+                    await DisplayAlert("Estudiante", "Estudiante Correcto", "OK");
+                }
+                else if (txtUsuario.Text != "null")
+                {
+                    //mensaje de validacion de usuario
+                    await DisplayAlert("Estudiante", "Estudiante InCorrecto", "OK");
+
+                    // await Navigation.PushAsync(new login());
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Mensaje de Advertencia", ex.Message, "OK");
+                //throw;
+            }
 
         }
     }
